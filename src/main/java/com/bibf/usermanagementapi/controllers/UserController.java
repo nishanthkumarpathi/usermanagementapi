ad887c9fef0d59c04017748c0cd367d93e953094
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-12-12 14:22:53 +0300

    Simple UserController Class

---
+++ src/main/java/com/bibf/usermanagementapi/controllers/UserController.java
@@ -0,0 +1,11 @@
+package com.bibf.usermanagementapi.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+// @RestController annotation is used to create RESTful web services using Spring MVC
+@RestController
+public class UserController {
+
+
+
+}
