26c77a5f70ef356122a377a942913b491225f9c5
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2023-12-12 14:44:21 +0300

    addUser Functionality - UserService is updated

--- src/main/java/com/bibf/usermanagementapi/controllers/UserController.java
+++ src/main/java/com/bibf/usermanagementapi/controllers/UserController.java
@@ -16,42 +16,5 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    /* GET ALL: Access by sending a GET request to http://localhost:8080/users */
-    @GetMapping("/users")
-    @ResponseStatus(HttpStatus.ACCEPTED) //not necessary as it is the default response.
-    public List<User> getUsers(){
-        return userService.getUsers();
-    }
-
-    /* GET ONE: Access by sending a GET request to http://localhost:8080/users/12 */
-    @GetMapping("/users/{id}")
-    public User getUser(@PathVariable("id") Integer userId){
-        return userService.getUser(userId);
-    }
-
-    /* ADD USER: Access by sending a POST request to http://localhost:8080/users with a JSON Body */
-    @PostMapping("/users")
-    @ResponseStatus(HttpStatus.CREATED)
-    public User addUser(@RequestBody User user){
-        return userService.addUser(user);
-    }
-
-    /* UPDATE USER: Access by sending a PUT request to http://localhost:8080/users/12 with a JSON Body */
-    @PutMapping("/users/{userId}")
-    public User updateUser(@PathVariable Integer userId, @RequestBody User user){
-        return userService.updateUser(userId,user);
-    }
-
-    /* DELETE USER: Access by sending a GET request to http://localhost:8080/users/12 with a JSON Body */
-    @DeleteMapping("/users/{userId}")
-    public String deleteUser(@PathVariable Integer userId){
-        return userService.deleteUser(userId);
-    }
-
-    /* GET USER BY EMAIL: Access by sending a GET request to http://localhost:8080/users/email/hamad@gmail.com */
-    @GetMapping("/users/email/{email}")
-    public User getUserByEmail(@PathVariable("email") String email){
-        return userService.getUserByEmail(email);
-    }
 
 }
--- src/main/java/com/bibf/usermanagementapi/services/UserService.java
+++ src/main/java/com/bibf/usermanagementapi/services/UserService.java
@@ -1,6 +1,8 @@
 package com.bibf.usermanagementapi.services;
 
 import com.bibf.usermanagementapi.models.User;
+import com.bibf.usermanagementapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,64 +12,12 @@ import java.util.List;
 @Service
 public class UserService {
 
-    // Step 1: Create a List of User Database using ArrayList or List
-    public static List<User> userDatabase = new ArrayList<>();
-    static {
-        userDatabase.add(new User(1, "Nishanth Kumar", "nishanth@gmail.comm", "password123"));
-        userDatabase.add(new User(2, "Maryam Ahmed", "maryam@gmail.com", "password456"));
-        userDatabase.add(new User(3, "Mark Johnson", "markjohnson@example.com", "password789"));
-        userDatabase.add(new User(4, "Sarah Lee", "sarahlee@example.com", "passwordabc"));
-        userDatabase.add(new User(5, "David Chen", "davidchen@example.com", "passworddef"));
-    }
+    @Autowired
+    private UserRepository userRepository;
 
-    // Step 2: Create a method to fetch all users from the database
-    public List<User> getUsers() {
-        // fetch all users and return them
-        return userDatabase;
-    }
-
-    // Step 3: Create a method to add the user to the database ArrayList
     public User addUser(User user) {
-        // add the user to the database and return the user
-        userDatabase.add(user);
-        return user;
-    }
-
-    // Step 4: updateUser method to update the user in the database
-
-    public User getUser(Integer userId) {
-        // fetch user from database
-        return userDatabase.stream()
-                .filter(user -> user.getId().equals(userId))
-                .findFirst()
-                .orElse(null);
-    }
-
-    public User updateUser(Integer userId, User user) {
-        // fetch user with userId, update it and return the new user
-        User existingUser = getUser(userId);
-        if(existingUser == null){return null;}
-        if(user.getName() != null) {existingUser.setName(user.getName());}
-        if(user.getEmail() != null) {existingUser.setEmail(user.getEmail());}
-        if(user.getPassword() != null) {existingUser.setPassword(user.getPassword());}
-        return existingUser;
-    }
-
-    public String deleteUser(Integer userId) {
-        // delete user with userId, return success.
-        if( userDatabase.removeIf(user -> userId.equals(user.getId())) ){
-            return "User with id " + userId + " deleted successfully!";
-        } else {
-            return "User with id " + userId + " not found";
-        }
-    }
-
-    public User getUserByEmail(String email) {
-        // get user by email from database
-        return userDatabase.stream()
-                .filter(user -> user.getEmail().equals(email))
-                .findFirst()
-                .orElse(null);
+        //ADDED: #1 The user repository save() method is easy!
+        return userRepository.save(user);
     }
 
 }
